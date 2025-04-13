a7d9fef077a31d49a7fbea997948bb0ca29fa2c0
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2025-04-13 16:55:26 -0300

    Adiciona novo schema para utilização via pub/sub redis

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.10-slim
 
-# Evita prompts
+ARG ENV=prod
+ENV ENV=$ENV
+
 ENV DEBIAN_FRONTEND=noninteractive
 
-# Instalar dependências do sistema
 RUN apt-get update && apt-get install -y \
     build-essential \
     cmake \
@@ -15,17 +16,14 @@ RUN apt-get update && apt-get install -y \
     git \
     && rm -rf /var/lib/apt/lists/*
 
-# Criar diretório da aplicação
 WORKDIR /app
 
-# Copiar requirements e instalar Python libs
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copiar a aplicação
 COPY app/ .
 
-# Expor porta do Flask
 EXPOSE 5000
 
-CMD ["python", "main.py"]
+RUN pip install watchdog
+CMD ["watchmedo", "auto-restart", "--directory=./", "--pattern=*.py", "--recursive", "--", "python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,52 +1,152 @@
-from flask import Flask, request, jsonify
-import face_recognition
+import redis
+import json
+import base64
 import numpy as np
 from PIL import Image
-
 from io import BytesIO
+import face_recognition
+import ast
 
-app = Flask(__name__)
-known_faces = {}
 
-@app.route('/encode-face', methods=['POST'])
-def register_face():
-    face_id = request.form.get("id")
-    image_file = request.files.get("image")
+def main():
+    print("Initializing")
+    r = redis.Redis(host='redis', port=6379, db=0)
+    pubsub = r.pubsub()
+    pubsub.subscribe('encode')
+    pubsub.subscribe('compare')
+    while True:
+        try:
+            for message in pubsub.listen():
+                match message['type']:
+                    case 'message':
+                        try:
+                            match message['channel']:
+                                case b'encode':
+                                    data = json.loads(message['data'])
+                                    match data.get('type'):
+                                        case 'face':
+                                            face_id = data['id']
+                                            image_b64 = data['image']
+                                            image_data = base64.b64decode(
+                                                image_b64)
+                                            image = Image.open(
+                                                BytesIO(image_data))
+                                            image_np = np.array(image)
+                                            encodings = face_recognition.face_encodings(
+                                                image_np)
+                                            if not encodings:
+                                                print(
+                                                    f"[{face_id}] ⚠️ No face detected.")
+                                                r.publish('face_encoded', json.dumps({
+                                                    "id": face_id,
+                                                    "status": "error",
+                                                    "message": "No face detected"
+                                                }))
+                                                continue
 
-    if not face_id or not image_file:
-        return jsonify({"error": "Missing id or image"}), 400
+                                            encoding_json = json.dumps(
+                                                encodings[0].tolist())
 
-    image = Image.open(image_file.stream)
-    image_np = np.array(image)
+                                            # Publica no canal 'face_encoded'
+                                            response = {
+                                                "id": face_id,
+                                                "encoding": encoding_json,
+                                                "status": "success"
+                                            }
 
-    encodings = face_recognition.face_encodings(image_np)
-    if not encodings:
-        return jsonify({"error": "No face found"}), 400
+                                            r.publish('face_encoded',
+                                                      json.dumps(response))
+                                            print(
+                                                f"[{face_id}] ✅ Face registrada com sucesso.")
+                                        case _:
+                                            print(data.get('type'),
+                                                  "unknown type")
+                                    break
+                                case b'compare':
+                                    data = json.loads(message['data'])
+                                    match(data.get('type')):
+                                        case 'face':
+                                            face_id = data['id']
+                                            image_b64 = data['image']
 
-    known_faces[face_id] = encodings[0].tolist()
-    return jsonify({"message": "Face registered", "id": face_id, 'enconding': known_faces[face_id]}), 200
+                                            # Decodifica a imagem base64
+                                            image_data = base64.b64decode(
+                                                image_b64)
+                                            image = Image.open(
+                                                BytesIO(image_data))
+                                            image_np = np.array(image)
 
-@app.route('/recognize', methods=['POST'])
-def recognize_face():
-    image_file = request.files.get("image")
+                                            encodings = face_recognition.face_encodings(
+                                                image_np)
+                                            if not encodings:
+                                            
+                                                r.publish('face_compared', json.dumps({
+                                                    "id": face_id,
+                                                    "status": "error",
+                                                }))
+                                                continue
 
-    if not image_file:
-        return jsonify({"error": "Missing image"}), 400
+                                            incoming_encoding = encodings[0]
 
-    image = Image.open(image_file.stream)
-    image_np = np.array(image)
+                                            # cursor = 0
+                                            matched_id = None
+                                            for k in r.keys():
+                                                value = r.get(k)
+                                                print(value)
+                                                if not value:
+                                                    continue
+                                                
+                                                decoded = json.loads(value)
+                                                print(decoded)
 
-    encodings = face_recognition.face_encodings(image_np)
-    if not encodings:
-        return jsonify({"error": "No face found"}), 400
+                                                # Verifica se o campo 'encoding' existe e não é None
+                                                encoding_str = decoded.get('encoding')
+                                                if encoding_str is None:
+                                                    print(f"[{k}] ⚠️ Codificação não encontrada. Pulando...")
+                                                    continue  # Pule este item, pois a codificação não está presente
+                                                
+                                                try:
+                                                    # Tenta converter a string da codificação em um array NumPy
+                                                    known_encoding = np.array(json.loads(encoding_str))  # Garantir que a codificação seja convertida de volta em array
+                                                    print(f"Known encoding: {known_encoding}")
+                                                except Exception as e:
+                                                    print(f"Erro ao converter a codificação para array NumPy: {e}")
+                                                    continue
 
-    unknown_encoding = encodings[0]
-    for face_id, known_encoding in known_faces.items():
-        match = face_recognition.compare_faces([known_encoding], unknown_encoding)[0]
-        if match:
-            return jsonify({"match": True, "id": face_id})
-    return jsonify({"match": False})
+                                                try:
+                                                    # Compare as codificações
+                                                    result = face_recognition.compare_faces([known_encoding], incoming_encoding, tolerance=0.6)
+                                                    print(f"Resultado da comparação: {result}")
+                                                    
+                                                    if result:  # Verifique se result não está vazio
+                                                        if result[0]:  # Se houver correspondência
+                                                            print('MATCH!')
+                                                            matched_id = k.decode().split(':')[1]
+                                                            break
+                                                    else:
+                                                        print("Nenhum resultado de comparação encontrado.")
+                                                except Exception as e:
+                                                    print(f"Erro ao comparar as codificações: {e}")
+                                                    continue
+                             
+
+                                            print(face_id, matched_id)
+                                            r.publish('face_compared', json.dumps({
+                                                "id": face_id,
+                                                "status": "not_found" if not matched_id else "success",
+                                                "matched_id": matched_id.replace('face-', '') if matched_id else None,
+                                            }))
+                                    break
+
+                        except Exception as e:
+                            print(
+                                f"[{face_id}] ❌ Erro ao processar mensagem:", e)
+
+        except KeyboardInterrupt:
+            print(
+                "\n🛑 Reloading...")
+            break
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    main()
