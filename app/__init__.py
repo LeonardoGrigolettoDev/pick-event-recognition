8907003c118cb332cfd2112ad0aa36ec86163c37
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2025-05-20 21:25:00 -0300

    Adiciona reorganização e criação de classes

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-import base64
-import numpy as np
-import cv2
-
-def b64_to_image(base64_string: str):
-    img_data = base64.b64decode(base64_string)
-    np_arr = np.frombuffer(img_data, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    return img
--- app/face.py
+++ app/face.py
@@ -6,6 +6,7 @@ import numpy as np
 import face_recognition
 import json
 import ast
+from app import base
 
 
 class FaceAI:
