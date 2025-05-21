7ea51725a35e6971b01d9811bf4ca7a84b0165b7
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2025-05-20 21:24:23 -0300

    Adiciona reorganização e criação de classes

---
+++ app/face.py
@@ -0,0 +1,81 @@
+import base64
+from PIL import Image
+from io import BytesIO
+import redis
+import numpy as np
+import face_recognition
+import json
+import ast
+
+
+class FaceAI:
+    def __init__(self):
+        pass
+
+    def read_face(self, image_b64):
+        """
+        Reads a face from the image and returns the face data.
+        """
+        image_data = base64.b64decode(
+            image_b64)
+        image = Image.open(
+            BytesIO(image_data))
+        image_np = np.array(image)
+        encodings = face_recognition.face_encodings(
+            image_np)
+        if not encodings:
+            return None
+
+        encoding_json = json.dumps(
+            encodings[0].tolist())
+        return encoding_json
+
+    def recognize_face(self, face: str, r):
+        """
+        Recognizes the face and returns the result.
+        """
+        image_data = base64.b64decode(face)
+        image = Image.open(BytesIO(image_data))
+        image_np = np.array(image)
+
+        encodings = face_recognition.face_encodings(
+            image_np)
+        if not encodings:
+            return None
+
+        incoming_encoding = encodings[0]
+
+        matched_id = None
+        for k in r.keys():
+            value = r.get(k)
+            if not value:
+                continue
+
+            decoded = json.loads(value)
+
+            encoding_str = decoded.get('encoding')
+            if encoding_str is None:
+                continue
+
+            try:
+                known_encoding = np.array(json.loads(encoding_str))
+            except Exception as e:
+                print(
+                    f"Error on convertion to array NumPy: {e}")
+                continue
+
+            try:
+                # Compare as codificações
+                result = face_recognition.compare_faces(
+                    [known_encoding], incoming_encoding, tolerance=0.6)
+
+                if result:  # Verifique se result não está vazio
+                    if result[0]:  # Se houver correspondência
+                        print('Found a match.')
+                        matched_id = k.decode().split(':')[1]
+                        break
+                print("Not found.")
+            except Exception as e:
+                print(f"Erro ao comparar as codificações: {e}")
+                continue
+        return matched_id
--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from PIL import Image
 from io import BytesIO
 import face_recognition
 import ast
+from face import FaceAI
 
 
 def main():
@@ -14,129 +15,58 @@ def main():
     pubsub = r.pubsub()
     pubsub.subscribe('encode')
     pubsub.subscribe('compare')
+    face_ai = FaceAI()
     while True:
         try:
             for message in pubsub.listen():
                 match message['type']:
                     case 'message':
                         try:
-                            match message['channel']:
-                                case b'encode':
-                                    data = json.loads(message['data'])
-                                    match data.get('type'):
-                                        case 'face':
-                                            face_id = data['id']
-                                            image_b64 = data['image']
-                                            image_data = base64.b64decode(
-                                                image_b64)
-                                            image = Image.open(
-                                                BytesIO(image_data))
-                                            image_np = np.array(image)
-                                            encodings = face_recognition.face_encodings(
-                                                image_np)
-                                            if not encodings:
-                                                print(
-                                                    f"[{face_id}] ⚠️ No face detected.")
-                                                r.publish('face_encoded', json.dumps({
-                                                    "id": face_id,
-                                                    "status": "error",
-                                                    "message": "No face detected"
-                                                }))
-                                                continue
+                            data = json.loads(message['data'])
+                            encode = message['channel'] == b'encode'
+                            match data.get('type'):
+                                case 'facial':
+                                    face_id = data['id']
+                                    image_b64 = data['image']
+                                    if (not image_b64):
+                                        print(
+                                            f"Invalid data received: {data}")
+                                        r.publish('face_encoded', json.dumps({
+                                            "id": face_id,
+                                            "status": "error",
+                                            "message": "No image received"
+                                        }))
+                                        continue
+                                    result = face_ai.read_face(
+                                        image_b64) if encode else face_ai.recognize_face(image_b64, r)
+                                    if not result:
+                                        print(
+                                            f"No face detected in image.")
+                                        r.publish('face_encoded' if encode else 'face_compared', json.dumps({
+                                            "id": face_id,
+                                            "status": "error",
+                                            "message": "No result found"
+                                        }))
+                                        continue
 
-                                            encoding_json = json.dumps(
-                                                encodings[0].tolist())
+                                    # Publica no canal 'face_encoded'
+                                    response = {
+                                        "id": face_id,
+                                        "encoding": result,
+                                        "status": "success"
+                                    } if encode else {
+                                        "id": face_id,
+                                        "status": "not_found" if not result else "success",
+                                        "matched_id": result.replace('face-', '') if result else None,
+                                    }
 
-                                            # Publica no canal 'face_encoded'
-                                            response = {
-                                                "id": face_id,
-                                                "encoding": encoding_json,
-                                                "status": "success"
-                                            }
-
-                                            r.publish('face_encoded',
-                                                      json.dumps(response))
-                                            print(
-                                                f"[{face_id}] ✅ Face registrada com sucesso.")
-                                        case _:
-                                            print(data.get('type'),
-                                                  "unknown type")
-                                    break
-                                case b'compare':
-                                    data = json.loads(message['data'])
-                                    match(data.get('type')):
-                                        case 'face':
-                                            face_id = data['id']
-                                            image_b64 = data['image']
-
-                                            # Decodifica a imagem base64
-                                            image_data = base64.b64decode(
-                                                image_b64)
-                                            image = Image.open(
-                                                BytesIO(image_data))
-                                            image_np = np.array(image)
-
-                                            encodings = face_recognition.face_encodings(
-                                                image_np)
-                                            if not encodings:
-                                            
-                                                r.publish('face_compared', json.dumps({
-                                                    "id": face_id,
-                                                    "status": "error",
-                                                }))
-                                                continue
-
-                                            incoming_encoding = encodings[0]
-
-                                            # cursor = 0
-                                            matched_id = None
-                                            for k in r.keys():
-                                                value = r.get(k)
-                                                print(value)
-                                                if not value:
-                                                    continue
-                                                
-                                                decoded = json.loads(value)
-                                                print(decoded)
-
-                                                # Verifica se o campo 'encoding' existe e não é None
-                                                encoding_str = decoded.get('encoding')
-                                                if encoding_str is None:
-                                                    print(f"[{k}] ⚠️ Codificação não encontrada. Pulando...")
-                                                    continue  # Pule este item, pois a codificação não está presente
-                                                
-                                                try:
-                                                    # Tenta converter a string da codificação em um array NumPy
-                                                    known_encoding = np.array(json.loads(encoding_str))  # Garantir que a codificação seja convertida de volta em array
-                                                    print(f"Known encoding: {known_encoding}")
-                                                except Exception as e:
-                                                    print(f"Erro ao converter a codificação para array NumPy: {e}")
-                                                    continue
-
-                                                try:
-                                                    # Compare as codificações
-                                                    result = face_recognition.compare_faces([known_encoding], incoming_encoding, tolerance=0.6)
-                                                    print(f"Resultado da comparação: {result}")
-                                                    
-                                                    if result:  # Verifique se result não está vazio
-                                                        if result[0]:  # Se houver correspondência
-                                                            print('MATCH!')
-                                                            matched_id = k.decode().split(':')[1]
-                                                            break
-                                                    else:
-                                                        print("Nenhum resultado de comparação encontrado.")
-                                                except Exception as e:
-                                                    print(f"Erro ao comparar as codificações: {e}")
-                                                    continue
-                             
-
-                                            print(face_id, matched_id)
-                                            r.publish('face_compared', json.dumps({
-                                                "id": face_id,
-                                                "status": "not_found" if not matched_id else "success",
-                                                "matched_id": matched_id.replace('face-', '') if matched_id else None,
-                                            }))
-                                    break
+                                    r.publish('face_encoded' if encode else 'face_compared',
+                                              json.dumps(response))
+                                    print(
+                                        f"[{face_id}] ✅ Face processed.")
+                                case _:
+                                    print(data.get('type'),
+                                          "unknown type")
 
                         except Exception as e:
                             print(
